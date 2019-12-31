b1636b5f652be2f3afcefa80cbc3d7f3b32904fd
Alan Nguyen <nguyenalan@berkeley.edu>
2019-12-31 14:29:18 -0800

    skeleton

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid, Responsive } from "semantic-ui-react";
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+// use grid layout for 
+
+const Home = () => {
+    return (
+        <Wrapper>
+            <Grid>
+
+            </Grid>
+        </Wrapper>
+    )
+}
+
+export default Home;
---
+++ src/components/Splash.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+
+// homepage with routes
+
+const Wrapper = styled.div``;
+
+const Splash = () => {
+}
+
+export default Splash;
