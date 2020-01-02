f48510681c4ea688c4908eb260577c0eda1ce746
Alan Nguyen <nguyenalan@berkeley.edu>
2020-01-02 13:49:34 -0800

    routes skeleton

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -6,6 +6,9 @@ import styled from "styled-components";
 import { Icon } from "semantic-ui-react";
 import Projects from "../assets/projects.json";
 
+// pages
+import { ClothSim } from "../routes";
+
 // todo: routes, project components
 
 // css - todo: animations/hover
@@ -36,6 +39,17 @@ const Link = styled.a`
   color: inherit;
 `;
 
+const routes = [
+  {
+    path: "/clothsim",
+    sidebar: () => (
+      <div>
+        <ClothSim />
+      </div>
+    )
+  }
+];
+
 const Splash = () => {
   useEffect(() => {
     anime({
@@ -47,52 +61,59 @@ const Splash = () => {
     });
   }, []);
   return (
-    <Wrapper>
-      <Content>
-        <Header>alan nguyen</Header>
-        <Text>
-          recently studied computer science at UC Berkeley. currently, i'm
-          interested in computer graphics and{" "}
-          <Link href="https://en.wikipedia.org/wiki/Creative_coding">
-            creative coding
-          </Link>
-          {""}
-          —particularly their interactions with digital media (music, images).
-        </Text>
-        <Text>
-          in my spare time, i shoot lots of{" "}
-          <Link href="https://alandn.me">
-            <u>photos</u>
-          </Link>
-          , watch too many{" "}
-          <Link href="https://soundcloud.com/stylate">
-            <u>movies</u>
-          </Link>
-          , and make{" "}
-          <Link href="https://soundcloud.com/stylate">
-            <u>music</u>
-          </Link>
-          . sort of. i also read up a little bit too much on fashion.
-        </Text>
-        <Text size="small">previously >> akamai, fox networks</Text>
-        <Text size="small">
-          cv >>{" "}
-          <Link href="/">
-            <u>here</u>
-          </Link>
-        </Text>
-        <Text size="small">
-          <Link href="https://github.com/stylate">
-            <Icon name="github" size="large" />
-          </Link>
-          <Link href="https://www.linkedin.com/in/alandn/">
-            <Icon name="linkedin" size="large" />
-          </Link>
-        </Text>
-        <Text>projects >></Text>
-        <Items items={Projects} />
-      </Content>
-    </Wrapper>
+    <Router>
+      <Wrapper>
+        <Content>
+          <Header>alan nguyen</Header>
+          <Text>
+            recently studied computer science at UC Berkeley. currently, i'm
+            interested in computer graphics and{" "}
+            <Link href="https://en.wikipedia.org/wiki/Creative_coding">
+              creative coding
+            </Link>
+            {""}
+            —particularly their interactions with digital media (music, images).
+          </Text>
+          <Text>
+            in my spare time, i shoot lots of{" "}
+            <Link href="https://alandn.me">
+              <u>photos</u>
+            </Link>
+            , watch too many{" "}
+            <Link href="https://soundcloud.com/stylate">
+              <u>movies</u>
+            </Link>
+            , and make{" "}
+            <Link href="https://soundcloud.com/stylate">
+              <u>music</u>
+            </Link>
+            . sort of. i also read up a little bit too much on fashion.
+          </Text>
+          <Text size="small">previously >> akamai, fox networks</Text>
+          <Text size="small">
+            cv >>{" "}
+            <Link href="/">
+              <u>here</u>
+            </Link>
+          </Text>
+          <Text size="small">
+            <Link href="https://github.com/stylate">
+              <Icon name="github" size="large" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/alandn/">
+              <Icon name="linkedin" size="large" />
+            </Link>
+          </Text>
+          <Text>projects >></Text>
+          <Items items={Projects} />
+        </Content>
+        <Switch>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} children={<route.sidebar />} />
+          ))}
+        </Switch>
+      </Wrapper>
+    </Router>
   );
 };
 
--- src/routes/clothsim.js
+++ src/routes/clothsim.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Grid, Image } from 'semantic-ui-react';
+import React from "react";
+import styled from "styled-components";
+import { Grid, Container, Header, Image } from "semantic-ui-react";
 
-const ClothSim = () => {
+const Wrapper = styled.div``;
 
-}
-
-export default ClothSim;
+export const ClothSim = () => (
+    <Wrapper>
+        <Container>
+            <Header as='h2'>Cloth Simulator</Header>
+            <p>
+                test
+            </p>
+        </Container>
+    </Wrapper>
+);
