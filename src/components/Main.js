9a44e09a17e108917c7cea3b01b4a0d0dad6ed7c
Alan Nguyen <nguyenalan@berkeley.edu>
2020-01-02 14:50:06 -0800

    update routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,22 +12,36 @@ const routes = [
     output: () => <Home />
   },
   {
+    path: "/hex",
+    output: () => {
+      window.location.href =
+        "https://www.notion.so/Audio-Visualization-with-three-js-0d7117978b29484e8f356625d70e1cbd";
+      return null;
+    }
+  },
+  {
     path: "/clothsim",
     output: () => <ClothSim />
-  }
+  },
+  {
+    path: "/crawler",
+    output: () => {
+      window.location.href =
+        "https://github.com/stylate/AudioCrawler";
+      return null;
+    }
+  },
 ];
 
 export const Main = () => (
-  <div>
-    <Switch>
-      {routes.map((route, index) => (
-        <Route
-          key={index}
-          path={route.path}
-          exact={route.exact}
-          children={<route.output />}
-        />
-      ))}
-    </Switch>
-  </div>
+  <Switch>
+    {routes.map((route, index) => (
+      <Route
+        key={index}
+        path={route.path}
+        exact={route.exact}
+        children={<route.output />}
+      />
+    ))}
+  </Switch>
 );
